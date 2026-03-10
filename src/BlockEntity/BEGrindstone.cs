c35e3ccc178eef6d68ffe48e3037f11a963e78ee
AartHauk <andy111111111111111@gmail.com>
2026-03-09 22:10:41 -0600

    Massive lang file simplification

--- src/BlockEntity/BEGrindstone.cs
+++ src/BlockEntity/BEGrindstone.cs
@@ -370,7 +370,14 @@ namespace Grindstones
 
 		public override void GetBlockInfo (IPlayer forPlayer, StringBuilder dsc)
 		{
-			string wheel = inventory[0].GetStackName() ?? Lang.Get("none");
+			string wheel = Lang.Get("none");
+			if (HasWheel)
+			{
+				string stone = Lang.Get("game:rock-" + inventory[0].Itemstack.Item.Variant["stone"]);
+				string metal = Lang.Get("game:item-rod-" + inventory[0].Itemstack.Item.Variant["metal"]);
+
+				wheel = Lang.Get("{0} Grinding Wheel with {1}", stone, metal);
+			}
 
 			dsc.AppendLine(Lang.Get("Wheel: {0}", wheel));
 		}
